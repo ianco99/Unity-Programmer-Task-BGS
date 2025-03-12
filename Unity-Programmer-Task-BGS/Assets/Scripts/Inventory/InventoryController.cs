bf5773659b19307b14aa7d6d6417409f30228619
Ian Kuznicki <iankuz25@gmail.com>
2025-03-12 19:49:20 -0300

    Created InventoryController class

---
+++ Unity-Programmer-Task-BGS/Assets/Scripts/Inventory/InventoryController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController
+{
+    
+    public void AddItem()
+    {
+
+    }
+
+    public void RemoveItem()
+    {
+
+    }
+
+    public void ClearItems()
+    {
+
+    }
+}
