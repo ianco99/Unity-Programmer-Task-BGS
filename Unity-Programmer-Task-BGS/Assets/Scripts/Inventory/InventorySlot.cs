294da68bf8af837f16b39f196ea90498d3961c44
Ian Kuznicki <iankuz25@gmail.com>
2025-03-12 23:56:24 -0300

    Implemented highlight when hovering a slot

--- Unity-Programmer-Task-BGS/Assets/Scripts/Inventory/InventorySlot.cs
+++ Unity-Programmer-Task-BGS/Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,11 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private ISlottable slottable;
     
     [SerializeField] private Image slotImage;
+    [SerializeField] private GameObject blinkImage;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Enter " + gameObject.name);
+        ToggleHover(true);
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Exit " + gameObject.name);
+        ToggleHover(false);
+        //throw new System.NotImplementedException();
+    }
+    
+    private void ToggleHover(bool value)
+    {
+        blinkImage.SetActive(value);
+    }
 }
