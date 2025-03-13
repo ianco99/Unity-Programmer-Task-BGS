b6cb9fc73043aed8513cc88f9520b48da5482f4b
Ian Kuznicki <iankuz25@gmail.com>
2025-03-13 05:22:05 -0300

    Player smoothly rotates towards movement direction

--- Unity-Programmer-Task-BGS/Assets/Scripts/Player/PlayerController.cs
+++ Unity-Programmer-Task-BGS/Assets/Scripts/Player/PlayerController.cs
@@ -7,19 +7,30 @@ namespace BGS.Player
     {
         [SerializeField] private CharacterController _cc;
         [SerializeField] private float _playerSpeed;
+        [SerializeField] private float _turnSmoothVelocity;
+        [SerializeField] private float _turnSmoothTime;
 
-        private Vector3 currentMotion;
+        private Vector3 _currentMotion;
 
         private void Update()
         {
-            _cc.SimpleMove(GetRelativeMovement() * _playerSpeed);
+
+            if (_currentMotion.magnitude >= 0.1f)   //brackeys 3d cc
+            {
+                float targetAngle = Mathf.Atan2(_currentMotion.x, _currentMotion.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(_cc.transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+                _cc.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                _cc.Move(moveDir.normalized * _playerSpeed * Time.deltaTime);
+            }
         }
 
         private void OnMove(InputValue action)
         {
             Vector2 actionValue = action.Get<Vector2>();
 
-            currentMotion = new Vector3(actionValue.x, 0, actionValue.y);
+            _currentMotion = new Vector3(actionValue.x, 0, actionValue.y);
         }
 
         private Vector3 GetRelativeMovement()
@@ -32,7 +43,7 @@ namespace BGS.Player
             cameraRight.y = 0f;
             cameraRight = cameraRight.normalized;
 
-            return cameraForward * currentMotion.z + cameraRight * currentMotion.x;
+            return cameraForward * _currentMotion.z + cameraRight * _currentMotion.x;
         }
     }
 }
